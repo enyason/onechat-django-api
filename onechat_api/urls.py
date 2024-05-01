9055debe2a3a28ce72c1615ce3c60e596ef0cbb8
Emmanuel Enya <enyasonjnr@gmail.com>
2024-05-01 20:31:37 +0100

    set up static files

--- onechat_api/urls.py
+++ onechat_api/urls.py
@@ -17,9 +17,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from onechat_api import settings
+from django.conf.urls.static import static
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('users.urls'), name="Users"),
     path('api/', include('authentication.urls'), name="Authentication"),
     path('api/', include('chat.urls'), name="Chat"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
